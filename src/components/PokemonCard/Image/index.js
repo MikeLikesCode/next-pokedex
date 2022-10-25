edc34a7a0155c91b1ddd3e3a715c7e1a17fe45d8
mikelikescode <mguerrero.codes@gmail.com>
2022-10-25 00:06:05 -0400

    Adding some research comments

--- src/components/PokemonCard/Image/index.js
+++ src/components/PokemonCard/Image/index.js
@@ -1,10 +1,19 @@
 export default function PokemonImage({ images, name }) {
+
+  const parsedImages = JSON.parse(images)
+  let default_image = parsedImages.front_default;
+
+  // Add some handling to check if the pokemon is male or female and find images for that
+
+  // This also applies for GMAX or any other variant versions of pokemon ("Starter", "World-Cap")
+
+
   return (
     <div>
       <img
+        className="w-[96px]"
         src={
-          JSON.parse(images)
-            .front_default
+          default_image
         }
         alt={name}
       />
